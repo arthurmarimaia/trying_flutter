b298c53976648b2097ed26c8259893d69d8b8992
unknown <arthurmarimaia@gmail.com>
2026-02-24 12:45:51 -0300

    base limpa flutter web + android

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tamagotchi'),
+      ),
+      body: const Center(
+        child: Text(
+          'Projeto iniciado',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
